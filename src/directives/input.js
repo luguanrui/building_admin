c30fde65e34245e3255836ac21db9f2306e0a6de
lujing@zhixuezhen.com <lujing@zhixuezhen.com>
2020-12-22 14:11:03 +0800

    update

--- src/directives/input.js
+++ src/directives/input.js
@@ -9,19 +9,45 @@ const spaceFilter = function(el) {
   })
 }
 
+// 防抖
+let debounce = (fn, delay) => {
+  var delay = delay || 300
+  var timer
+  return function() {
+    var th = this
+    var args = arguments
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(function() {
+      timer = null
+      fn.apply(th, args)
+    }, delay)
+  }
+}
+
 // 限制只能输入整数和小数（适用于价格类、最多两位小数）
 const priceFilter = function(el) {
-  addListener(el, 'input', () => {
-    el.value = el.value.match(/^\d*(\.?\d{0,2})/g)[0] || null
-    if (isNaN(el.value)) {
-      el.value = ''
-    }
-  })
+  addListener(
+    el,
+    'keyup',
+    debounce(() => {
+      //添加防抖 方便添加小数点
+      el.value = el.value.match(/^\d*(\.?\d{0,2})/g)[0] || null
+      if (isNaN(el.value)) {
+        el.value = ''
+      }
+      //格式化去掉却没有输入小数位的小数点
+      el.value = +el.value
+      //触发input事件
+      el.dispatchEvent(new Event('input'))
+    })
+  )
 }
 
 // 限制只能输入字母数字（适用于运单号）
 const integerLetterFilter = function(el) {
-  addListener(el, 'input', () => {
+  addListener(el, 'keyup', () => {
     el.value = el.value.replace(/[\W]/g, '')
     el.dispatchEvent(new Event('input'))
   })

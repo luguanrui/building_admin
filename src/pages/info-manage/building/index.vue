33e38d76646dba7bae30528c50d76aa8e1f48335
luguanrui <1481744045@qq.com>
2021-01-03 17:01:15 +0800

    update

--- src/pages/info-manage/building/index.vue
+++ src/pages/info-manage/building/index.vue
@@ -1,55 +1,32 @@
 <template>
   <a-card>
     <div class="building-wrapper">
-      <div class="building-item" v-for="build in buildHomeList" :key="build.id">
-        <div class="left-wrapper">
-          <!-- image -->
-          <img :src="imageParese(build.buildingPic)" alt="" />
-          <!-- <img :src="image"/> -->
-        </div>
-        <div class="right-wrapper">
-          <a-form-model :label-col="labelCol" :wrapper-col="wrapperCol">
+      <a-row type="flex" class="building-item" v-for="build in buildHomeList" :key="build.id">
+        <a-col flex="280px" style="margin-right: 30px;">
+          <img :src="image" width="100%" />
+          <!-- <img :src="imageParese(build.buildingPic)" alt="楼宇图片" width="100%" /> -->
+        </a-col>
+        <a-col flex="auto">
+          <a-row style="margin-bottom: 50px">
             <h3 class="title">楼宇信息</h3>
-            <a-form-model-item label="楼宇名称">
-              <span class="detail-text">{{ build.name }}</span>
-            </a-form-model-item>
-            <a-form-model-item label="建设单位">
-              <span class="detail-text">{{ build.constructCompany }}</span>
-            </a-form-model-item>
-            <a-form-model-item label="地址">
-              <span class="detail-text">{{ build.address }}</span>
-            </a-form-model-item>
-            <a-form-model-item label="负责人">
-              <span class="detail-text">{{ build.contactName }}</span>
-            </a-form-model-item>
-            <a-form-model-item label="联系电话">
-              <span class="detail-text">{{ build.contactPhone }}</span>
-            </a-form-model-item>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 7, offset: 1 }"> 楼宇名称：{{ build.name }} </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 7, offset: 1 }"> 建设单位：{{ build.constructCompany }} </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 7, offset: 1 }"> 地址：{{ build.address }} </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 7, offset: 1 }"> 负责人：{{ build.contactName }} </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 7, offset: 1 }"> 联系电话：{{ build.contactPhone }} </a-col>
+          </a-row>
+          <a-row>
             <h3 class="title">建筑面积</h3>
-            <a-form-model-item label="主楼">
-              <span class="detail-text">{{ build.mainArea }}&nbsp;&nbsp;平方米</span>
-            </a-form-model-item>
-            <a-form-model-item label="企业">
-              <span class="detail-text">{{ build.mainCompanyArea }}&nbsp;&nbsp;平方米</span>
-            </a-form-model-item>
-            <a-form-model-item label="单身公寓">
-              <span class="detail-text">{{ build.singleApartmentArea }}&nbsp;&nbsp;平方米</span>
-            </a-form-model-item>
-            <a-form-model-item label="空置">
-              <span class="detail-text">{{ build.mainAreaLeft }}&nbsp;&nbsp;平方米</span>
-            </a-form-model-item>
-            <a-form-model-item label="副楼">
-              <span class="detail-text">{{ build.viceArea }}&nbsp;&nbsp;平方米</span>
-            </a-form-model-item>
-            <a-form-model-item label="企业">
-              <span class="detail-text">{{ build.viceCompanyArea }}&nbsp;&nbsp;平方米</span>
-            </a-form-model-item>
-            <a-form-model-item label="空置">
-              <span class="detail-text">{{ build.viceAreaLeft }}&nbsp;&nbsp;平方米</span>
-            </a-form-model-item>
-          </a-form-model>
-        </div>
-      </div>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 4, offset: 2 }"> 主楼：{{ build.mainArea }}&nbsp;&nbsp;平方米 </a-col>
+            <a-col :xs="{ span: 11, offset: 1 }" :lg="{ span: 4, offset: 2 }"> 企业：{{ build.mainCompanyArea }}&nbsp;&nbsp;平方米 </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 4, offset: 2 }"> 单身公寓：{{ build.singleApartmentArea }}&nbsp;&nbsp;平方米 </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 4, offset: 2 }"> 空置：{{ build.mainAreaLeft }}&nbsp;&nbsp;平方米 </a-col>
+            <a-col :xs="{ span: 11, offset: 1 }" :lg="{ span: 4, offset: 2 }"> 副楼：{{ build.viceArea }}&nbsp;&nbsp;平方米 </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 4, offset: 2 }"> 企业：{{ build.viceCompanyArea }}&nbsp;&nbsp;平方米 </a-col>
+            <a-col :xs="{ span: 5, offset: 1 }" :lg="{ span: 4, offset: 2 }"> 空置：{{ build.viceAreaLeft }}&nbsp;&nbsp;平方米 </a-col>
+          </a-row>
+        </a-col>
+      </a-row>
     </div>
   </a-card>
 </template>
@@ -59,20 +36,11 @@
 .building-wrapper {
   .building-item {
     width: 100%;
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    .left-wrapper {
-      padding: 0 20px;
-    }
-    .right-wrapper {
-      flex: 1;
-      padding: 0 20px;
-      .title {
-        color: #000;
-        padding: 10px 0;
-        border-bottom: 1px solid #ccc;
-      }
+    margin-bottom: 30px;
+    .title {
+      color: #000;
+      padding: 10px 0;
+      border-bottom: 1px solid #ccc;
     }
   }
 }
